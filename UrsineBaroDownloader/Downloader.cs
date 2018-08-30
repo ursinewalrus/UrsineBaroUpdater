045fb609dc2513a32b4786d6006a0248f88f76a6
Josh Kerxhalli <jkerxhalli@litera.com>
2018-08-30 15:47:54 -0500

    works as intended, need to move where downloader connects to into web config though

--- UrsineBaroDownloader/Downloader.cs
+++ UrsineBaroDownloader/Downloader.cs
@@ -9,31 +9,97 @@ using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace UrsineBaroDownloader
 {
 
     class Downloader
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            Console.ReadKey();
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"http://localhost:12345/?download=1");
-            request.AutomaticDecompression = DecompressionMethods.GZip;
+            GetUpdateOptions();
+        }
 
-            using (var client = new HttpClient(new HttpClientHandler {
-                                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate }))
+        private static void GetUpdateOptions()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"http://localhost:12345/");
+
+            using (var client = new HttpClient(new HttpClientHandler { }))
             {
                 client.BaseAddress = new Uri("http://localhost:12345");
-                HttpResponseMessage response = client.GetAsync("?download=1").Result;
-                var responseBytes = response.Content.ReadAsByteArrayAsync().Result;
-                FileStream zipFile = new FileStream(@".\downloadedZip.zip", FileMode.Create);
-                zipFile.Write(responseBytes, 0, responseBytes.Length);
-                zipFile.Close();
-                ZipFile.ExtractToDirectory(@".\downloadedZip.zip",@".")
-                ;
-
+                HttpResponseMessage response = client.GetAsync("?download=getOptions").Result;
+                var responseString = response.Content.ReadAsStringAsync().Result;
+                List<string> possibleDownloads = responseString.Split('|').ToList();
+                Application.EnableVisualStyles();
+                Application.Run(new ZipSelectForm(possibleDownloads));
             }
         }
+
+        public static void GetSelectedOption(string zipFile)
+        {
+            using (var client = new HttpClient(new HttpClientHandler
+            {
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+            }))
+            {
+                client.BaseAddress = new Uri("http://localhost:12345");
+                HttpResponseMessage response = client.GetAsync("?download="+zipFile).Result;
+                var responseBytes = response.Content.ReadAsByteArrayAsync().Result;
+                FileStream downloadedZip = new FileStream(@".\tempFile.zip", FileMode.Create);
+                downloadedZip.Write(responseBytes, 0, responseBytes.Length);
+                downloadedZip.Close();
+
+                using (ZipArchive archive = ZipFile.OpenRead(@".\tempFile.zip"))
+                {
+                    foreach (ZipArchiveEntry entry in archive.Entries)
+                    {
+                        var splitPath = entry.FullName.Split('/');
+                        var fileDir = string.Join("/", splitPath.Take(splitPath.Length - 1).ToList());
+                        if (!Directory.Exists(fileDir) && fileDir != "")
+                        {
+                            Directory.CreateDirectory(fileDir);
+                        }
+                        fileDir = @".\" + fileDir + @"\";
+                        var extractPath = fileDir + entry.Name;
+                        entry.ExtractToFile(extractPath, true);
+                    }
+                }
+                File.Delete(@".\tempFile.zip");
+            }
+        }
+
+    }
+
+    public class ZipSelectForm : Form
+    {
+        public List<string> FormOptions;
+        public List<Button> Options;
+        public static string SelectedOption;
+        public ZipSelectForm(List<string> zipFiles)
+        {
+            FormOptions = zipFiles;
+            int yLoc = 0;
+            foreach(string option in FormOptions)
+            {
+                var button = new Button();
+                button.Size = new Size(200, 60);
+                button.Location = new Point(30, 30 + yLoc);
+                button.Text = option;   
+                this.Controls.Add(button);
+                SelectedOption = option;
+                button.Click += new EventHandler(OptionSelect);
+                //Options.Add(button);
+                yLoc += 90;
+            }
+        }
+
+        public static void OptionSelect(object sender, EventArgs e)
+        {
+            Downloader.GetSelectedOption((sender as Button).Text);
+        }
+
     }
 }
--- UrsineBaroServer/ServerStart.cs
+++ UrsineBaroServer/ServerStart.cs
@@ -23,36 +23,24 @@ namespace UrsineBaroServer
 
                 for (; ; )
                 {
-                    //https://stackoverflow.com/questions/13385633/serving-large-files-with-c-sharp-httplistener meebs
                     Console.WriteLine("Listening...");
 
                     HttpListenerContext context = listener.GetContext();
                     var Response = context.Response;
                     HttpListenerRequest request = context.Request;
-                    if (context.Request.QueryString["download"] == "1")
+                    if (context.Request.QueryString["download"] != null)
                     {
-                        var file = @".\fakeBaro.zip";
-                        using (FileStream fs = File.OpenRead(file))
+                        string qsArg = context.Request.QueryString["download"];
+                        if (qsArg == "getOptions")
                         {
-                            Response.ContentLength64 = fs.Length;
-                            Response.SendChunked = false;
-                            //Response.ContentType
-                            Response.AddHeader("Content-Disposition", "attachment; filename=baro.zip");
-                            var fLen = fs.Length;
-                            byte[] buffer = new byte[fLen];
-                            int read;
-                            using (BinaryWriter bw = new BinaryWriter(Response.OutputStream))
-                            {
-                                while ((read = fs.Read(buffer,0,buffer.Length)) > 0)
-                                {
-                                    bw.Write(buffer, 0, read);
-                                    //bw.Flush(); -> this makes no sense tome
-                                }
-                                bw.Close();
-                            }
-                            Response.StatusCode = (int)HttpStatusCode.Accepted;
-                            Response.StatusDescription = "OK";
-                            Response.OutputStream.Close();
+                            var ext = new List<string> { ".zip" };
+                            var files = Directory.GetFiles(@".\").Where(f => ext.Contains(Path.GetExtension(f))).ToList();
+                            string returnOptions = string.Join("|", files);
+                            SendSelectableOptions(Response, returnOptions);
+                        }
+                        else
+                        {
+                            SendSelectedZip(Response, qsArg);
                         }
                     }
                 }
@@ -60,6 +48,45 @@ namespace UrsineBaroServer
             }
         }
 
+        private static void SendSelectableOptions(HttpListenerResponse Response, string returnOptions)
+        {
+            Response.StatusCode = (int)HttpStatusCode.Accepted;
+            Response.StatusDescription = "OK";
+            Response.SendChunked = false;
+            Response.AddHeader("Content-Disposition", "text/plain");
 
+            byte[] buffer = Encoding.UTF8.GetBytes(returnOptions);
+            Response.ContentLength64 = buffer.Length;
+            Stream output = Response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            output.Close();
+        }
+
+        private static void SendSelectedZip(HttpListenerResponse Response, string zipFile)
+        {
+            using (FileStream fs = File.OpenRead(zipFile))
+            {
+                Response.ContentLength64 = fs.Length;
+                Response.SendChunked = false;
+                Response.AddHeader("Content-Disposition", "attachment; filename=baro.zip");
+
+                var fLen = fs.Length;
+                byte[] buffer = new byte[fLen];
+                int read;
+
+                using (BinaryWriter bw = new BinaryWriter(Response.OutputStream))
+                {
+                    while ((read = fs.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        bw.Write(buffer, 0, read);
+                        //bw.Flush(); -> this makes no sense tome
+                    }
+                    bw.Close();
+                }
+                Response.StatusCode = (int)HttpStatusCode.Accepted;
+                Response.StatusDescription = "OK";
+                Response.OutputStream.Close();
+            }
+        }
     }
 }
